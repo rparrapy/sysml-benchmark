69be480240b90d0b2e0a8ea48ed806223f04d341
Hadoop <hadoop@ibm-power-1.dima.tu-berlin.de>
2016-09-08 20:41:54 +0200

    felix_first_commit

--- sysml-benchmark-bundle/src/main/resources/utils/sync-sysml.sh
+++ sysml-benchmark-bundle/src/main/resources/utils/sync-sysml.sh
@@ -6,7 +6,7 @@ PEELROOT=$( cd $SCRIPTPATH; cd ..; pwd )
 # Github data
 USER=stratosphere
 REPO=incubator-systemml
-BRANCH=staging
+BRANCH=flink-lr
 
 ARCHIVE_NAME=$BRANCH.zip
 URL="https://github.com/$USER/$REPO/archive/$ARCHIVE_NAME"
@@ -33,11 +33,12 @@ unzip $ARCHIVE_NAME
 # build systemml
 echo "Building SystemML..."
 cd $FOLDER_NAME
-mvn clean package -DskipTests
+mvn -DskipTests -Dhadoop.version=2.7.1 -T1.0C clean package
 
 # copy SystemML.jar and scripts to apps
 
-cp "target/SystemML.jar" "$PEELROOT/apps"
-cp -r "scripts/" "$PEELROOT/apps"
+mkdir "$PEELROOT/apps"
+cp "target/SystemML.jar" "$PEELROOT/apps/"
+cp -r "scripts" "$PEELROOT/apps/"
 
 
